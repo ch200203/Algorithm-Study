57721f2fc8d3425a889513afe00c3b1b3cf8e31d
ch200203 <ch200205@naver.com>
2021-06-23 23:05:19 +0900

    study 23day

--- src/This_Is_Coding_Test/Part02/Impl_01.java
+++ src/This_Is_Coding_Test/Part02/Impl_01.java
@@ -1,5 +1,10 @@
 package This_Is_Coding_Test.Part02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 
 // 상하좌우
 // N x N 크기의 정사각형 공간에서 이동
@@ -10,8 +15,42 @@ package This_Is_Coding_Test.Part02;
 // => 최종적으로 도착할 좌표를 구하여라
 
 public class Impl_01 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		int [] dx = {0, 0 ,-1, 1};
+		int [] dy = {-1, 1, 0 , 0};
 		
-	}
+		String [] move = {"L", "R", "U", "D"};
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		// String [] way = br.readLine().split(" ");
+		String [] way = {"R", "R", "R", "U", "D", "D"};
+		
+		int x = 0;
+		int y = 0;
+		
+		for (int i = 0; i < way.length; i++) {
+			for (int j = 0; j < move.length; j++) {
+				if(way[i] == move[j]) {
+					
+					int nx = x + dx[j];
+					int ny = y + dy[j];
+					
+					
+					// 공간을 벗어난 경우 실행하지 않음
+					if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+					
+					// 좌표이동 확정
+                    x = nx;
+                    y = ny;
+				}
+			}
+		}
+		
+		System.out.println((x + 1) + " " + (y + 1));
+
+	}// R R R U D D
 
 }
