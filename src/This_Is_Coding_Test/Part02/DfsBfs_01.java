8eb60a06e8a77abc0e3509875c192292b51c9773
ch200203 <ch200205@naver.com>
2021-07-10 23:18:49 +0900

    study

--- src/This_Is_Coding_Test/Part02/DfsBfs_01.java
+++ src/This_Is_Coding_Test/Part02/DfsBfs_01.java
@@ -1,7 +1,16 @@
 package This_Is_Coding_Test.Part02;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 // 음료수얼려먹기
 
 public class DfsBfs_01 {
+	 public static int N,M;
+	   public static int [][] graph = new int [1000][1000];
+	   
+	   public static void main(String[] args){
+		   BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+	   }
 
 }
