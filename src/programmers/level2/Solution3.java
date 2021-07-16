ebbeca83ce97c7ceece8e95d43facb39d1c7cf0f
ch200203 <ch200205@naver.com>
2021-07-16 22:37:52 +0900

    study lmc

---
+++ src/programmers/level2/Solution3.java
@@ -0,0 +1,21 @@
+package programmers.level2;
+
+// N개의 최소공배수 (LMC)
+// 정의를 확장해서, n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 
+// 가장 작은 숫자가 됩니다. n개의 숫자를 담은 배열 arr이 입력되었을 때 
+// 이 수들의 최소공배수를 반환하는 함수, solution을 완성해 주세요.
+// https://programmers.co.kr/learn/courses/30/lessons/12953
+
+
+
+public class Solution3 {
+	public static void main(String[] args) {
+		
+	}
+	
+	public static int solution(int[] arr){
+		
+		return 0;
+	}
+
+}
