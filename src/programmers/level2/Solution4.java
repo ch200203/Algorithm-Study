a77580d854975a36b714fa3e8958449f08ec22d6
ch200203 <ch200205@naver.com>
2021-07-22 22:45:23 +0900

    study

---
+++ src/programmers/level2/Solution4.java
@@ -0,0 +1,15 @@
+package programmers.level2;
+
+public class Solution4 {
+	public static void main(String[] args) {
+		int A [] =  {};
+		int B [] =  {};
+		
+		System.out.println(solution(A, B));
+	}
+	
+	public static int solution(int [] A, int [] B){
+		return 0;
+	}
+
+}
