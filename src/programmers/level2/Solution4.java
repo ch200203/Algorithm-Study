68e6868d27a54dd7d9fd75112dbe01bc52d11cb4
ch200203 <ch200205@naver.com>
2021-07-23 23:32:40 +0900

    study

--- src/programmers/level2/Solution4.java
+++ src/programmers/level2/Solution4.java
@@ -1,14 +1,23 @@
 package programmers.level2;
 
+// 최솟값 만들기
+// https://programmers.co.kr/learn/courses/30/lessons/12941
+
+
 public class Solution4 {
 	public static void main(String[] args) {
-		int A [] =  {};
-		int B [] =  {};
+		int A [] =  {1,2};
+		int B [] =  {3,4};
 		
 		System.out.println(solution(A, B));
 	}
 	
-	public static int solution(int [] A, int [] B){
+	public static int solution(int [] arr1, int [] arr2){
+
+		
+	
+		
+		
 		return 0;
 	}
 
