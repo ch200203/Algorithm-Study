2b3474116f90b5cc743a3d72d39760b6eab52a65
ch200203 <ch200205@naver.com>
2021-07-24 23:13:14 +0900

    study

--- src/programmers/level2/Solution4.java
+++ src/programmers/level2/Solution4.java
@@ -1,5 +1,7 @@
 package programmers.level2;
 
+import java.util.Arrays;
+
 // 최솟값 만들기
 // https://programmers.co.kr/learn/courses/30/lessons/12941
 
@@ -13,12 +15,18 @@ public class Solution4 {
 	}
 	
 	public static int solution(int [] arr1, int [] arr2){
-
 		
-	
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		int sum = 0;
+		
+		for(int i = 0; i < arr1.length; i++){
+			sum += arr1[i] * arr2[arr1.length-i - 1];
+		}
 		
 		
-		return 0;
+		return sum;
 	}
 
 }
