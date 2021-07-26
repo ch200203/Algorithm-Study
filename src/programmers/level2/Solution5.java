3b676a6b3953ac282f1c158c1d2a4eed511d6444
ch200203 <ch200205@naver.com>
2021-07-26 22:10:53 +0900

    study

--- src/programmers/level2/Solution5.java
+++ src/programmers/level2/Solution5.java
@@ -1,13 +1,32 @@
 package programmers.level2;
 
+import java.util.Arrays;
+
+// 더 맵게
+// https://programmers.co.kr/learn/courses/30/lessons/42626
+//섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+// Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때,
+// 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수
+
 public class Solution5 {
 	public static void main(String[] args) {
+		int [] scoville = {1,2,3,9,10,12};
+		int K = 7;
 		
+		System.out.println(solution(K, scoville));
 	
 	}
 	
-	public static void solution(){
+	public static int solution(int K, int [] scoville){
+		Arrays.sort(scoville);
 		
+		for (int i = 0; i < scoville.length; i++) {
+			if(scoville[i] < K){
+				scoville[i] = scoville[i] + (scoville[i + 1] * 2);
+				
+			}
+		}
+		return 0;
 	}
 	
 
