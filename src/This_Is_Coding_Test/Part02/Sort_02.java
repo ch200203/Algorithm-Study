3464ccc221d6c11e8c77b56de8e5eedcdbacb298
ch200203 <ch200205@naver.com>
2021-08-11 23:05:15 +0900

    Create Pare02_Sort_Test

---
+++ src/This_Is_Coding_Test/Part02/Sort_02.java
@@ -0,0 +1,15 @@
+package This_Is_Coding_Test.Part02;
+
+import java.util.Scanner;
+
+// 성적이 낮은 순서로 학생을 출력하기
+
+public class Sort_02 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		 
+		int n = scan.nextInt();
+		
+	}
+
+}
