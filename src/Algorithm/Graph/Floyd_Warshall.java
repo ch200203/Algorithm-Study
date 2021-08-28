5d638d5cc062c84fec9485f1ce7893f1fc7ba97b
ch200203 <ch200205@naver.com>
2021-08-28 10:18:59 +0900

    floyd warshall Algorithm

--- src/Algorithm/Graph/Floyd_Warshall.java
+++ src/Algorithm/Graph/Floyd_Warshall.java
@@ -1,5 +1,8 @@
 package Algorithm.Graph;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 // 참고 : https://pangsblog.tistory.com/90
 // https://www.acmicpc.net/problem/11404
@@ -8,7 +11,41 @@ package Algorithm.Graph;
 
 public class Floyd_Warshall {
 	public static int cityCount = 0;
+	public static int[][] distance;
+    public static final int INF = 1000000000;
+    
 	public static void main(String[] args) {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        cityCount = Integer.parseInt(br.readLine());
+        
+        int busCount = Integer.parseInt(br.readLine());
+        
+        distance = new int[cityCount+1][cityCount+1];
+        
+        for(int i=1; i <= cityCount; i++) {
+            for(int j=1; j <= cityCount; j++) {
+                if(i == j) continue;
+                distance[i][j] = INF;
+            }
+        }
+        
+        while(busCount-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int time = Integer.parseInt(st.nextToken());
+            
+            distance[start][end] = Math.min(distance[start][end], time);    
+        }
+ 
+        floydWarshall();
+        print();
+		
+		
 		
 	}
 
