2426f9995a3cbfcf134a1252eaaf7c1848c69a63
ch200203 <ch200205@naver.com>
2021-09-03 23:14:50 +0900

    level2 soluion8 add

--- src/programmers/level2/Solution8.java
+++ src/programmers/level2/Solution8.java
@@ -1,12 +1,17 @@
 package programmers.level2;
 
+// https://programmers.co.kr/learn/courses/30/lessons/42586
+// 스택 / 큐
+
+
 public class Solution8 {
 	public static void main(String[] args) {
 
 	}
 
-	public static void solution() {
-
+	public static int[] solution(int[] progresses, int[] speeds) {
+			int [] answer = {};
+			return answer;
 	}
 
 }
