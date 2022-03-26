7e2b3f13c9595c278bc269c5ac7cd604591cc2c6
ch200203 <ch200205@naver.com>
2022-03-27 02:16:19 +0900

    백준 11047

---
+++ src/baekjoon/Main_11047.java
@@ -0,0 +1,34 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+class Main_11047{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+
+        int coin [] = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            coin[i] = scan.nextInt();
+        }
+
+        int count = 0;
+        
+        //  N개의 동전 중 가장 큰 가치를 지닌 동전부터 탐색하여 동전의 가치가 K보다 클 경우는 넘어가고, 
+        // 아닐경우는 최대 구성 가능한 개수를 더해주면 된다. 
+        // ==> Greedy Algorithm
+        for (int i = n - 1; i >= 0 ; i--) {
+            // 현재 동전의 가치가 K 보다 작거나 같아야지 구성가능 하다.
+            if(coin[i] <= k) {
+                // 현재 가치의 동전으로 구성할 수 있는 개수를 더해준다.
+                count += (k / coin[i]);
+                k = k % coin[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
