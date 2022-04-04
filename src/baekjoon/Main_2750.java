aec1a2c1f871fa70a72c304031ee45a2cf2b7005
ch200203 <ch200205@naver.com>
2022-04-04 22:06:36 +0900

    code test

---
+++ src/baekjoon/Main_2750.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_2750 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int [] arr = new int [num];
+
+        for (int i = 0; i < num; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < num; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
