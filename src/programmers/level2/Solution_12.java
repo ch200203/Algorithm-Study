5c929535e9d56d4a71d765bc7f73b0c43f889c81
ch200203 <ch200205@naver.com>
2022-05-12 23:56:54 +0900

    기능개발 프로그래머스

---
+++ src/programmers/level2/Solution_12.java
@@ -0,0 +1,17 @@
+package programmers.level2;
+
+// 기능개발
+// https://programmers.co.kr/learn/courses/30/lessons/42586
+public class Solution_12 {
+    public static void main(String[] args) {
+        int[] progresses =  {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+
+        System.out.println(solution(progresses, speeds));
+    }
+
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        return answer;
+    }
+}
