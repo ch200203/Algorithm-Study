dec300a89fd67e399abbcbf54cbe4f4d4352bd49
ch200203 <ch200205@naver.com>
2022-08-27 15:14:49 +0900

    main 18452 java 저장

---
+++ src/baekjoon/silver/Main_18452.java
@@ -0,0 +1,23 @@
+package baekjoon.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_18452 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken()); // 도시의 갯수
+        int M = Integer.parseInt(st.nextToken()); // 도로의 갯수
+        int K = Integer.parseInt(st.nextToken()); // 거리정보
+        int X = Integer.parseInt(st.nextToken()); // 출발도시의 번호
+
+        
+        
+        int[][] arr = new int[N][2];
+
+    }
+}
+
