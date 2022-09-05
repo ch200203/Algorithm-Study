242d1f9e62505f5f885a3ccad134c67b08a43add
ch200203 <ch200205@naver.com>
2022-09-05 22:37:54 +0900

    DFS 재귀(스택) 구현, 문자열 뒤집기 기본 복습

--- src/inflearn/DFSBFS/Node.java
+++ src/inflearn/DFSBFS/Node.java
@@ -3,6 +3,7 @@ package inflearn.DFSBFS;
 public class Node {
     int data;
     Node lt, rt; // 객체주소 저장
+
     public Node(int val) {
         data = val;
         lt = rt = null;
@@ -12,21 +13,26 @@ public class Node {
 
 class Main {
     Node root;
-    public void DFS(Node rot) {
-
+    public void DFS(Node root) {
+        if(root == null) return; // root node가 null 인 경우 => 마지막 노드임
+        else {
+            // System.out.print("root.data :: " + root.data + " "); // 전위순회
+            DFS(root.lt);
+            // System.out.print("root.data :: " + root.data + " "); // 중위순회
+            DFS(root.rt);
+            // System.out.print("root.data :: " + root.data + " "); // 후위순회
+        }
     }
 
     public static void main(String args[]) {
         Main tree = new Main();
         tree.root = new Node(1);
         tree.root.lt = new Node(2);
-        tree.root.lt = new Node(3);
+        tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
         tree.root.rt.lt = new Node(6);
         tree.root.rt.rt = new Node(7);
-        tree.DFS(tree.root);
-
-
+        tree.DFS(tree.root); // root 노드
     }
 }
--- src/inflearn/chapter1/String_4.java
+++ src/inflearn/chapter1/String_4.java
@@ -1,20 +1,51 @@
 package inflearn.chapter1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class String_4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int N = scan.nextInt();
-
+        String[] str = new String[N];
+    
         for(int i = 0; i < N; i++){
-            String str = scan.nextLine();
-            solution(str);
+            str[i] = scan.next();
+        }
+
+        for(String x : solution(str, N)) {
+            System.out.println(x);
         }
         
     }
-    
-    public static String solution(String str) {
-        return null;
+
+    // 직접 뒤집는 알고리즘 구현
+    public static ArrayList<String> solution(String[] str, int n) {
+        ArrayList<String> answer=new ArrayList<>(); 
+        for(String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp=s[lt];
+				s[lt]=s[rt];
+				s[rt]=tmp;
+				lt++;
+				rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+        return answer;
+    }
+
+
+    // String Builder 사용방법
+    public static ArrayList<String> solution2(String[] str, int n) {
+        ArrayList<String> answer = new ArrayList<String>();
+        for(String x : str) {
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+        return answer;
     }
 }
