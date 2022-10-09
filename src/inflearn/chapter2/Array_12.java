67362e1a4663a074bfc05d6a3a7f995dce06393c
ch200203 <ch200205@naver.com>
2022-10-09 23:22:31 +0900

    코테 배열 12번

--- src/inflearn/chapter2/Array_12.java
+++ src/inflearn/chapter2/Array_12.java
@@ -1,5 +1,6 @@
 package inflearn.chapter2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -8,18 +9,41 @@ public class Array_12 {
         Scanner scan = new Scanner(System.in);
 
         int n = scan.nextInt();
-        int[][] arr = new int[n+1][6];
+        int m = scan.nextInt();
+        int[][] arr = new int[m][n];
         
-        // i : 학생 번호
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= 5; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 arr[i][j] = scan.nextInt();
             }
         }
 
-        solution(n,arr);
+        solution(n, m, arr);
     }
 
-    public static void solution(int n, int[][] arr) {
+    public static void solution(int n, int m, int[][] arr) {
+        int answer = 0;
+        System.out.println(Arrays.deepToString(arr));
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                int cnt = 0;
+                for (int k = 0; k < m; k++) {
+                    int pi = 0, pj = 0;
+                    for (int s = 0; s < n; s++) {
+                        // k 회차 시험의 i번, j번 학생의 등수 확인
+                        if(arr[k][s] == i) pi = s;
+                        if(arr[k][s] == j) pj = s;
+                    }
+                    
+                    // 학생수 확인
+                    if(pi < pj) cnt++;
+                }
+
+                if(cnt == m) answer++;
+            }
+        }
+        System.out.println(answer);
     }
 }
+
