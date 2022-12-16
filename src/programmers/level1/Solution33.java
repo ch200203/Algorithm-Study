bcd53a3fb2485dde5da889104a492dad16070e12
ch200203 <ch200205@naver.com>
2022-12-16 09:29:51 +0900

    .

--- src/programmers/level1/Solution33.java
+++ src/programmers/level1/Solution33.java
@@ -1,22 +1,86 @@
 package programmers.level1;
 
-/**
- * 콜라문제 
- * https://school.programmers.co.kr/learn/courses/30/lessons/132267
- */
+import java.util.ArrayList;
+
 public class Solution33 {
-    public int solution(int a, int b, int n) {
+    public static void main(String[] args) {
+        int[] ingredient //= {1, 2, 3, 1};
+        // ={2, 1, 1, 2, 3, 1, 2, 3, 1};
+        = {1, 3, 2, 1, 2, 3, 1, 3, 1, 2};
+        System.out.println(solution_2(ingredient));
+
+    }
+    // 시간 초과
+    private static int solution_2(int[] ingredient) {
         int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        if(ingredient.length < 4) return 0;
+        for (int i : ingredient) sb.append(i);
 
+        String str = sb.toString();
+        while(str.contains("1231")) {
+            str = str.replaceFirst("1231", "");
+            answer++;
+        }       
+
+        return answer;
+    }
+
+    public static int solution(int[] ingredient) {
+        // 1 : 빵, 2 : 야채, 3: 고기
+        int answer = 0;
+        ArrayList<Integer> hamList = new ArrayList<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
+
+        // 1,2,3,1 이 들어오는 경우만 햄버거를 만들 수 있음
+        resultList.add(1);
+        resultList.add(2);
+        resultList.add(3);
+        resultList.add(1);
         
-        while(true) {
-            if(n <  a) break;
+        for (int i = 0; i < ingredient.length; i++) {
+            hamList.add(ingredient[i]);
 
-            answer += (n / a) * b;
-            int remain = n % a;
-            n = (n / a) * b + remain;
+            // 4개 이상인 경우만 판단
+            if(hamList.size() >= 4) {
+                // 비교를 위한 임시 리스트 생성
+                ArrayList<Integer> tmpList = new ArrayList<>();
+                tmpList.add(hamList.get(hamList.size() - 4));
+                tmpList.add(hamList.get(hamList.size() - 3));
+                tmpList.add(hamList.get(hamList.size() - 2));
+                tmpList.add(hamList.get(hamList.size() - 1));
+
+                if(tmpList.equals(resultList)) {
+                    answer++;
+                    
+                    // 햄버거가 만들어진 경우 List에서 제외
+                    hamList.remove(hamList.size() - 1);
+                    hamList.remove(hamList.size() - 1);
+                    hamList.remove(hamList.size() - 1);
+                    hamList.remove(hamList.size() - 1);
+                }
+            }
         }
 
+        return answer;
+    }
+
+    // 배열 stack 사용한 풀이법 
+    // 좋은 풀이법!
+    public int solution_3(int[] ingredient) {
+        int[] stack = new int[ingredient.length];
+        int sp = 0;
+        int answer = 0;
+        for (int i : ingredient) {
+            stack[sp++] = i;
+            if (sp >= 4 && stack[sp - 1] == 1
+                && stack[sp - 2] == 3
+                && stack[sp - 3] == 2
+                && stack[sp - 4] == 1) {
+                sp -= 4;
+                answer++;
+            }
+        }
         return answer;
     }
 }
