efa923523517105d021f2110739ea106ee23870f
ch200203 <ch200205@naver.com>
2022-12-23 22:43:09 +0900

    solve : inflearn 조합수 (메모제이션) 재귀 풀이

---
+++ src/inflearn/chapter8/dfs_7.java
@@ -0,0 +1,27 @@
+package inflearn.chapter8;
+
+import java.util.Scanner;
+
+/**
+ * 조합의 경우수(조합수) - 메모제이션
+ * 아래의 공식을 사용하여 재귀를 이용해 조합수를 구하시오
+ * nCr = n-1Cr-1 + n-1Cr
+ */
+public class dfs_7 {
+    static int[][] dy = new int[35][35];
+
+    private static int DFS(int n, int r){
+        if(dy[n][r] > 0 ) return dy[n][r];
+        if(n == r || r == 0) return 1;
+        else return dy[n][r] = DFS(n - 1, r - 1) + DFS(n -1, r);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int r = scan.nextInt();
+
+        System.out.println(DFS(n, r));
+
+        scan.close();
+    }
+}
