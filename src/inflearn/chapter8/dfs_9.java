4d7a1d7f2d929f3762c01caf64e617248241200f
ch200203 <ch200205@naver.com>
2023-01-01 21:57:53 +0900

    inflean : 조합수 구하기 DFS

--- src/inflearn/chapter8/dfs_9.java
+++ src/inflearn/chapter8/dfs_9.java
@@ -2,18 +2,40 @@ package inflearn.chapter8;
 
 import java.util.Scanner;
 
+/**
+ * 조합수 구하기
+ * 1 ~ N 까지의 번호가 적힌 구슬 있습니다
+ * 이 중 M 개를 뽑는 정답의 수를 구하는 프로그램을 작성하세요
+ * N(3<=N<=10), M(2<=M<=N)
+ */
 public class dfs_9 {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int[][] arr = new int[7][7];
-
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                arr[i][j] = scan.nextInt();
+    static int[] combi;
+    static int n, m;
+    /**
+     * @param L level
+     * @param s start index
+     */
+    private static void DFS(int L, int s) {
+        if(L == m) {
+            for(int x : combi) System.out.print(x + " ");
+            System.out.println();
+        } else {
+            for(int i = s; i <= n; i ++) {
+                combi[L] = i;
+                DFS(L + 1, i + 1);
             }
         }
-        scan.close();
 
+    }
+    public static void main(String[] args) {
         
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        m = scan.nextInt();
+        combi = new int[m];
+        
+        DFS(0, 1);
+
+        scan.close();
     }
 }
