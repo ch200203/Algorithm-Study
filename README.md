dbce2c0d9d0495e0cfd16947885f3952bd6c6457
ch200203 <ch200205@naver.com>
2023-02-15 23:12:01 +0900

    readme : 커밋메시지 추가

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
  - 백준 
  - 프로그래머스
  - 인프런(java 코딩테스트 강의)
+
+
+### 커밋메시지
+- solve : 
+    - programmers lvN 문제명  
+    - boj_문제번호 (알고리즘 종류)  
+- inflearn : 강의명
--- src/Main2.java
+++ src/Main2.java
@@ -2,25 +2,23 @@ import java.util.Arrays;
 
 public class Main2 {
 	public static void main(String[] args) {
-		int money = 4578;
-		int [] costs =  {1, 4, 99, 35, 50, 1000};
-		solution(money, costs);
-	}
-	
-	
-	// 아마 그리디 인듯?
-	public static int solution(int money, int[] costs) {
-        int answer = 0;
-        int coin_num [] = new int [costs.length];
-        
-        
-        for (int i = 0; i < costs.length; i++) {
-			
-		}
-        
-        
-        
-        return answer;
-    }
+		int k = 1;
+		int day = 6;
 
+		System.out.println(Arrays.toString(apartmentManagement(day, k)));
+	}
+
+	private static int[] apartmentManagement(int day, int k) {
+        int[] result = new int[12];
+        int currentDay = day;
+        for (int i = 0; i < 12; i++) {
+            if (currentDay == 5 || currentDay == 6) {
+                result[i] = 1;
+            } else {
+                result[i] = 0;
+            }
+            currentDay = (currentDay + k) % 7;
+        }
+        return result;
+    }
 }
