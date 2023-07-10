636d343f6d57e2fccb3f71fd736181a72c5389e8
ch200203 <ch200205@naver.com>
2023-07-10 23:23:19 +0900

    solve : boj_1546 java bronze

---
+++ src/baekjoon/Bronze/Main_1546.java
@@ -0,0 +1,28 @@
+package baekjoon.Bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_1546 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        long max = 0;
+        double sum = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            int score = Integer.parseInt(st.nextToken());
+            // max = Math.max(max, score);
+            if(score > max) max = score;
+            sum += score;
+        }
+
+        System.out.println(((sum / max) * 100.0) / N);
+    }
+
+}
